cdab178dbaf52df8c0546c5d9f171f1f4766f2d0
tlaze <tomlazore@gmail.com>
2021-09-23 22:16:01 -0400

    Calendar dynamically updates color throughout the day

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 var todaysDate = $('#currentDay');
+var hourSlot = $('textarea');
+var time = moment().format('H');
 
 
 
@@ -6,6 +8,29 @@ function displayTime(){
     var rightNow = moment().format('dddd MMM Do YYYY');
     todaysDate.text(rightNow);
 }
+
+var hour = $(".hour").text().trim();
+
+//Loops through each hour
+//If color changes based on the tim
+hourSlot.each(function(i){
+
+    if(time === i+9){
+        $(this).addClass('present');
+    }
+    else if(time > i+9){
+        $(this).addClass('past');
+    }
+    else if(time < i+9){
+        $(this).addClass('future');
+    }
+    else{
+        console.log("Time does not exist");
+    }
+
+
+})
+    
 
 
 displayTime();
