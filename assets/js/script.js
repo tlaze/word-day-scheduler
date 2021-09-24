f4c2488ac382428440895fe7fdb3e1a416ff50a1
tlaze <tomlazore@gmail.com>
2021-09-23 22:31:47 -0400

    CSS updated so user inputs is centered in each slot

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,11 @@
 var todaysDate = $('#currentDay');
 var hourSlot = $('textarea');
 var time = moment().format('H');
-
-
-
-function displayTime(){
-    var rightNow = moment().format('dddd MMM Do YYYY');
-    todaysDate.text(rightNow);
-}
-
 var hour = $(".hour").text().trim();
 
+
 //Loops through each hour
-//If color changes based on the tim
+//Color changes dynamically based on the time of day
 hourSlot.each(function(i){
 
     if(time === i+9){
@@ -27,10 +20,20 @@ hourSlot.each(function(i){
     else{
         console.log("Time does not exist");
     }
+});
 
 
-})
+//Uses moment.js to display time
+function displayTime(){
+    var rightNow = moment().format('dddd MMM Do YYYY');
+    todaysDate.text(rightNow);
+}
+
+function saveEntry(){
+    // localStorage.setItem('userInputs', hourSlot.val());
+}
+
+
+
     
-
-
 displayTime();
