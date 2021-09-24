d132a970962a3443cc7baae4edba3133bf04f7b8
tlaze <tomlazore@gmail.com>
2021-09-24 00:56:04 -0400

    One textarea goes to local storage when every button is clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,15 @@ var todaysDate = $('#currentDay');
 var hourSlot = $('textarea');
 var time = moment().format('H');
 var hour = $(".hour").text().trim();
+var inputButton = $('.saveBtn');
+var ex = $('9');
+
+var savedInputs = new Array();
 
 
 //Loops through each hour
 //Color changes dynamically based on the time of day
+
 hourSlot.each(function(i){
 
     if(time === i+9){
@@ -23,6 +28,9 @@ hourSlot.each(function(i){
 });
 
 
+inputButton.click('click', saveEntry);
+
+
 //Uses moment.js to display time
 function displayTime(){
     var rightNow = moment().format('dddd MMM Do YYYY');
@@ -30,10 +38,36 @@ function displayTime(){
 }
 
 function saveEntry(){
-    // localStorage.setItem('userInputs', hourSlot.val());
+    console.log("click");
+    // deleteEntry();
+    // getStoredEntries();
+
+
+    savedInputs.push({
+        entry: $('#9').val().trim(),
+        entry: $('#10').val().trim(),
+        entry: $('#11').val().trim(),
+        entry: $('#12').val().trim(),
+        entry: $('#13').val().trim(),
+        entry: $('#14').val().trim(),
+        entry: $('#15').val().trim(),
+        entry: $('#16').val().trim(),
+        entry: $('#17').val().trim(),
+    });
+    localStorage.setItem('Event', JSON.stringify(savedInputs));
+    console.log(localStorage);
+
+
+}
+function deleteEntry(){
+    localStorage.clear();
 }
 
-
-
-    
+function getStoredEntries(){
+    var entry = localStorage.getItem('Event');
+    if(entry != null){
+        savedInputs = JSON.parse(entry);
+    }
+}
+ 
 displayTime();
